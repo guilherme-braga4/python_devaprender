d97710b8ebf0684018462e95550d5a3af42145c1
guilherme-braga <90586912+guilherme-braga4@users.noreply.github.com>
2021-11-16 20:49:45 +0000

    Initial commit

---
+++ aula1.py
@@ -0,0 +1,23 @@
+# Variáveis
+# criar varíaveis: letra minúscula e _ pra separar palavras
+
+#Números
+#contador =  10
+
+#Valores Boleanos -> true/false ou 0/1
+  #True e False precisam começar maiúsculos
+#set_Port = True
+
+#Strings -> frase/combiação de palavras ou letras
+#Nome = "guilherme"
+
+#Desafio 1: Programa que retorna Valor/Hora através das entradas: Horas trabalhadas e Salário Mensal
+
+horas_trabalhadas = int(input("Digite a quantidade de horas trabalhadas: "))
+salario = int(input("Digite o salario mensal: "))
+
+valor_hora =  salario / horas_trabalhadas 
+
+print(valor_hora)
+
+
