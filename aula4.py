29b267504b213e91471876359a308cb9b7d62149
guilherme-braga <90586912+guilherme-braga4@users.noreply.github.com>
2021-11-17 16:23:43 +0000

    aula 4 - pronta

---
+++ aula4.py
@@ -0,0 +1,20 @@
+#coleções de listas
+
+idade = [18,19,25,26] #array de idades
+
+total = 0 #valor inicial é sempre necessário
+
+'''print(idade[1]) #acessando um índice específico'''
+
+# a lista recebe qualque valor: inteiro, string ou boleano
+
+#Desafio: Somar os valores idades e imprimir a soma
+
+#soma é igual a idade, basicamente atribuimos uma varíavel para acessar a constante
+
+for soma in idade:
+  total = total + soma
+  '''qual será o processamento''' 
+print(total)
+
+  #pode escolher se quer colocar o print dentro ou fora do laço de repetição
